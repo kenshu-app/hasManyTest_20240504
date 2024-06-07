93ab0672555852a506561e3c6923f15ef4d6fac1
kenshu <kenshu.app@gmail.com>
2024-06-07 15:16:43 +0900

    update: index.blade and Pagenation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,16 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Category;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        $collection = Product::where('category_id', '=', 1)->where('price', '>=', 10)->where(function ($q)
-        {
-            $q->where('name', 'LIKE', '%チョコ%');
-            $q->orWhere('description', 'LIKE', '%チョコ%');
-        })->get();
-        return view('index', ['products' => $collection]);
+        // $collection = Product::where('category_id', '=', 1)->where('price', '>=', 10)->where(function ($q)
+        // {
+        //     $q->where('name', 'LIKE', '%チョコ%');
+        //     $q->orWhere('description', 'LIKE', '%チョコ%');
+        // })->get();
+        // return view('index', ['products' => $collection]);
+        return view('index', [
+            'products'   => Product::with('category')->paginate(9),
+            'categories' => Category::all()
+        ]);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="ja">
 
 <head>
     <meta charset="utf-8">
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.72.0">
-    <title>Album example · Bootstrap</title>
+    <title>{{ config('app.name') }}</title>
     <link rel="canonical" href="https://v5.getbootstrap.com/docs/5.0/examples/album/">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
@@ -19,19 +19,23 @@
         integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
     <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
+        body {
+            line-height: 2;
         }
 
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
+        .price {
+            margin-left: 1em;
+            font-size: .5em;
+            border: 1px solid #ccc;
+            padding: .2em .5em;
+            border-radius: 0.15em;
+        }
+
+        .card-text {
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 3;
+            overflow: hidden;
         }
     </style>
 </head>
@@ -42,17 +46,17 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-8 col-md-7 py-4">
-                        <h4 class="text-white">ReskillPHP研修</h4>
-                        <p class="text-muted">Add some information about the album below, the author, or any other
-                            background context. Make it a few sentences long so folks can pick up some informative
-                            tidbits. Then, link them off to some social networking sites or contact information.</p>
+                        <h4 class="text-white">お菓子一覧</h4>
+                        <p class="text-muted">
+                            当サイトで紹介しているお菓子は個人的なおススメとして、コンビニなどで人気のある商品や古くから駄菓子屋で愛されているお菓子です。子供から大人まで、手軽に安価に満足いただけるラインナップです。
+                        </p>
                     </div>
                     <div class="col-sm-4 offset-md-1 py-4">
-                        <h4 class="text-white">研修科目</h4>
+                        <h4 class="text-white">詳細リンク</h4>
                         <ul class="list-unstyled">
-                            <li><a href="#" class="text-white">Linux/Git</a></li>
-                            <li><a href="#" class="text-white">HTML/CSS/JS</a></li>
-                            <li><a href="#" class="text-white">PHP/MySQL/Laravel</a></li>
+                            <li><a href="" class="text-white"><i class="fa fa-wpforms"></i> マイページ</a></li>
+                            <li><a href="" class="text-white"><i class="fa fa-pie-chart"></i> 他の人のお菓子</a></li>
+                            <li><a href="" class="text-white"><i class="fa fa-heart"></i> お気に入りの一覧</a></li>
                         </ul>
                     </div>
                 </div>
@@ -61,7 +65,8 @@
         <div class="navbar navbar-dark bg-dark shadow-sm">
             <div class="container">
                 <a href="#" class="navbar-brand d-flex align-items-center">
-                    <i class="fa fa-pie-chart" aria-hidden="true"></i><strong class="ml-2">Products</strong>
+                    <i class="fa fa-pie-chart" aria-hidden="true"></i><strong
+                        class="ml-2">{{ config('app.name') }}</strong>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader"
                     aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
@@ -74,13 +79,31 @@
         <section class="py-5 text-center container">
             <div class="row py-lg-5">
                 <div class="col-lg-6 col-md-8 mx-auto">
-                    <h1 class="font-weight-light">Products Sample</h1>
-                    <p class="lead text-muted">Something short and leading about the collection below—its contents, the
-                        creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it
-                        entirely.</p>
+                    <h1 class="font-weight-light">{{ config('app.name') }}一覧</h1>
+                    <p class="lead text-muted">
+                        甘くて美味しいお菓子が勢揃い！チョコレートやクッキー、アイスや和菓子など、幅広いラインナップをご用意しています。どのお菓子も一口食べれば幸せな気分になれること間違いなしです。</p>
                     <form action="" method="post" novalidate class="row g-1">
-                            <label class="col-3"><a href="#" class="btn btn-secondary">絞り込み検索</a></label>
-                            <label class="col-9"><input type="text" class="form-control"></label>
+                        <span class="col-4 text-left border-bottom border-gray-200"><i class="fa fa-list"></i>
+                            カテゴリ</span>
+                        <label class="col-8">
+                            <select name="" class="form-select">
+                                <option selected>カテゴリ</option>
+                                @foreach ($categories as $category)
+                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                @endforeach
+                            </select>
+                        </label>
+                        <span class="col-4 text-left border-bottom border-gray-200"><i class="fa fa-money"></i>
+                            価格範囲</span>
+                        <label class="col-4"><input type="text" name="" class="form-control"
+                                placeholder="最安値(円)"></label>
+                        <label class="col-4"><input type="text" name="" class="form-control"
+                                placeholder="最高値(円)"></label>
+                        <span class="col-4 text-left border-bottom border-gray-200"><i class="fa fa-tags"></i>
+                            キーワード</span>
+                        <label class="col-8"><input type="text" name="" class="form-control"
+                                placeholder="キーワード"></label>
+                        <a class="col-8 ml-auto btn btn-secondary" href="#">絞り込み検索</a>
                     </form>
                 </div>
             </div>
@@ -94,20 +117,28 @@
                                 <img src="https://picsum.photos/id/{{ $product->category_id }}/600/320" width="100%"
                                     height="225" alt="dammyImg">
                                 <div class="card-body">
-                                    <h4 class="h-4 text-black-50">{{ $product->name }}</h4>
-                                    <p class="card-text">This is a wider card with supporting text below as a natural
-                                        lead-in to additional content. This content is a little bit longer.</p>
+                                    <h4 class="h-4 text-black-50 border-bottom border-secondary pb-3">
+                                        {{ $product->name }} <span class="price">{{ $product->price }}円</span></h4>
+                                    <p class="card-text">{{ $product->description }}</p>
                                     <div class="d-flex justify-content-between align-items-center">
                                         <div class="btn-group">
-                                            <a class="btn btn-sm btn-outline-secondary" target="_blank" href="https://www.google.com/search?q={{ $product->name }}"><i class="fa fa-search"></i> 詳細検索</a>
-                                            <a class="btn btn-sm btn-outline-secondary" target="_blank" href="https://www.amazon.co.jp/{{ $product->name }}/s?k={{ $product->name }}"><i class="fa fa-amazon"></i> Amazon購入</a>
+                                            <a class="btn btn-sm btn-outline-secondary" target="_blank"
+                                                href="https://www.google.com/search?q={{ $product->name }}"><i
+                                                    class="fa fa-search"></i> 詳細検索</a>
+                                            <a class="btn btn-sm btn-outline-secondary" target="_blank"
+                                                href="https://www.amazon.co.jp/{{ $product->name }}/s?k={{ $product->name }}"><i
+                                                    class="fa fa-amazon"></i> Amazon購入</a>
                                         </div>
-                                        <small class="text-muted">9 mins</small>
+                                        <small
+                                            class="text-muted text-white-50 badge bg-secondary">{{ $product->category->name }}</small>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     @endforeach
+                </div>
+                <div class="col-12 bg-light pb-0 mt-4">
+                    {{ $products->links() }}
                 </div>
             </div>
         </div>
@@ -117,8 +148,8 @@
             <p class="float-right mb-1"><a href="#" class="text-white-50">Back to top</a></p>
             <p class="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
             <p class="mb-0">New to Bootstrap? <a href="/" class="text-white-50">Visit the homepage</a> or
-                read
-                our <a href="/docs/5.0/getting-started/introduction/" class="text-white-50">getting started guide</a>.
+                read our <a href="/docs/5.0/getting-started/introduction/" class="text-white-50">getting started
+                    guide</a>.
             </p>
         </div>
     </footer>
